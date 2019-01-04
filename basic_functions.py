168a3bcc14425abe6572d344872016a2afb0cdc9
edenbrok <e.j.l.denbrok@student.tudelft.nl>
2019-01-04 11:21:37 +0100

    transition and sensor model dataframes

--- basic_functions.py
+++ basic_functions.py
@@ -7,10 +7,36 @@ Created on Sat Dec 29 22:05:40 2018
 
 import json
 import os
+import pandas as pd
 
 
 #the names of all the issues in the domain for later use
 issues = ["Fruit", "Juice", "Topping1", "Topping2"]
+
+
+transition_model = pd.DataFrame({'St-1': ["Sc", "Sh", "St", "Sr"],
+                                 'P(Sc)': [1, 0, 0, 0],
+                                 'P(Sh)': [0, 1, 0, 0],
+                                 'P(St)': [0, 0, 1, 0],
+                                 'P(Sr)': [0, 0, 0, 1]})
+
+transition_model.set_index('St-1', inplace = True)
+
+sensor_model = pd.DataFrame({'St': ["Sc", "Sh", "St", "Sr"],
+                                 'P(Mc)': [0, 0, 0, 0],
+                                 'P(Mf)': [0, 0, 0, 0],
+                                 'P(Mu)': [0, 0, 0, 0],
+                                 'P(Ms)': [0, 0, 0, 0],
+                                 'P(Msi)': [0, 0, 0, 0]})
+
+sensor_model.set_index('St', inplace = True)
+
+move_count = pd.DataFrame({'St': ["Sc", "Sh", "St", "Sr"],
+                                 'Mc': [0, 0, 0, 0],
+                                 'Mf': [0, 0, 0, 0],
+                                 'Mu': [0, 0, 0, 0],
+                                 'Ms': [0, 0, 0, 0],
+                                 'Msi': [0, 0, 0, 0]})
 
 
 #a function that returns the utility of a certain bid given a preference profile
@@ -43,31 +69,33 @@ def type_of_move(bid, prevbid, pref1, pref2):
 logs = os.listdir("./logs/training_logs")
 
 #open the first one
-with open(("./logs/training_logs/%s" % logs[0]), "r") as read_file:
-    data = json.load(read_file)
-
-#get the preference profiles of both agents
-pref1 = data["Utility1"]
-pref2 = data["Utility2"]
-
-
-#calculate the utility of agent 1's bid for both agents
-for turn in data["bids"]:
-    if "accept" in turn:
-        break
-    util_1 = calc_util(turn["agent1"], pref1)
-    util_2 = calc_util(turn["agent1"], pref2)
-    print("Agent 1 util: ",util_1," Agent 2 util: ",util_2)
-
-#This prints the type of moves agent 1 is making
-for i in range(len(data["bids"])):
-    if i > 0:
-        if "accept" in data["bids"][i]:
-            break       
-        current_round = data["bids"][i]
-        prev_round = data["bids"][i-1]
-        print(type_of_move(current_round["agent1"], prev_round["agent1"],
-                           pref1, pref2))
+for log in logs:
+    with open(("./logs/training_logs/%s" % log), "r") as read_file:
+        data = json.load(read_file)
+    
+    print(log)
+    #get the preference profiles of both agents
+    pref1 = data["Utility1"]
+    pref2 = data["Utility2"]
+    
+    
+    #calculate the utility of agent 1's bid for both agents
+    for turn in data["bids"]:
+        if "accept" in turn:
+            break
+        util_1 = calc_util(turn["agent1"], pref1)
+        util_2 = calc_util(turn["agent1"], pref2)
+        #print("Agent 1 util: ",util_1," Agent 2 util: ",util_2)
+    
+    #This prints the type of moves agent 1 is making
+    for i in range(len(data["bids"])):
+        if i > 0:
+            if "accept" in data["bids"][i]:
+                break       
+            current_round = data["bids"][i]
+            prev_round = data["bids"][i-1]
+            #print(type_of_move(current_round["agent1"], prev_round["agent1"],
+            #                   pref1, pref2))
         
         
     
