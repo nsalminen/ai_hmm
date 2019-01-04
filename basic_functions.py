27b8020e45cc0740b5cc837b1593ab2eafc20875
edenbrok <e.j.l.denbrok@student.tudelft.nl>
2019-01-04 12:54:09 +0100

    filling sensor model table + log fix

--- basic_functions.py
+++ basic_functions.py
@@ -31,12 +31,14 @@ sensor_model = pd.DataFrame({'St': ["Sc", "Sh", "St", "Sr"],
 
 sensor_model.set_index('St', inplace = True)
 
-move_count = pd.DataFrame({'St': ["Sc", "Sh", "St", "Sr"],
-                                 'Mc': [0, 0, 0, 0],
-                                 'Mf': [0, 0, 0, 0],
-                                 'Mu': [0, 0, 0, 0],
-                                 'Ms': [0, 0, 0, 0],
-                                 'Msi': [0, 0, 0, 0]})
+move_count = pd.DataFrame({'Strategy': ["conceder", "hardheaded", "tft", "random"],
+                                 'concession': [0, 0, 0, 0],
+                                 'fortunate': [0, 0, 0, 0],
+                                 'unfortunate': [0, 0, 0, 0],
+                                 'selfish': [0, 0, 0, 0],
+                                 'silent': [0, 0, 0, 0]})
+
+move_count.set_index('Strategy', inplace = True)
 
 
 #a function that returns the utility of a certain bid given a preference profile
@@ -64,6 +66,15 @@ def type_of_move(bid, prevbid, pref1, pref2):
         return "silent"
     else:
         return "ERROR"
+    
+def return_strategies(filename):
+    useful, trash = filename.split(".")
+    strategies= useful.split("_")
+    if not strategies[1].isalpha():
+        strategies[1] = strategies[1][:-1]
+    return strategies
+
+    
 
 #get all the logs
 logs = os.listdir("./logs/training_logs")
@@ -73,30 +84,47 @@ for log in logs:
     with open(("./logs/training_logs/%s" % log), "r") as read_file:
         data = json.load(read_file)
     
-    print(log)
+    s1, s2 = return_strategies(log)
     #get the preference profiles of both agents
     pref1 = data["Utility1"]
     pref2 = data["Utility2"]
     
-    
-    #calculate the utility of agent 1's bid for both agents
-    for turn in data["bids"]:
-        if "accept" in turn:
-            break
-        util_1 = calc_util(turn["agent1"], pref1)
-        util_2 = calc_util(turn["agent1"], pref2)
-        #print("Agent 1 util: ",util_1," Agent 2 util: ",util_2)
-    
-    #This prints the type of moves agent 1 is making
+
+    #moves agent 1 is making
     for i in range(len(data["bids"])):
         if i > 0:
             if "accept" in data["bids"][i]:
                 break       
             current_round = data["bids"][i]
             prev_round = data["bids"][i-1]
-            #print(type_of_move(current_round["agent1"], prev_round["agent1"],
-            #                   pref1, pref2))
-        
-        
+            move = (type_of_move(current_round["agent1"], prev_round["agent1"],
+                              pref1, pref2))
+            move_count.loc[s1, move] += 1
+    
+    #moves agent 2 is making        
+    for i in range(len(data["bids"])):
+        if i > 0:
+            if "accept" in data["bids"][i]:
+                break       
+            current_round = data["bids"][i]
+            prev_round = data["bids"][i-1]
+
+            move = (type_of_move(current_round["agent2"], prev_round["agent2"],
+                              pref2, pref1))
+            move_count.loc[s2, move] += 1
+            
+
+
+tempSc = move_count.loc["conceder"]/move_count.loc["conceder"].sum()
+tempSh = move_count.loc["hardheaded"]/move_count.loc["hardheaded"].sum()
+tempSt = move_count.loc["tft"]/move_count.loc["tft"].sum()
+tempSr = move_count.loc["random"]/move_count.loc["random"].sum()
+
+sensor_model.loc["Sc"] = tempSc.values
+sensor_model.loc["Sh"] = tempSh.values
+sensor_model.loc["St"] = tempSt.values 
+sensor_model.loc["Sr"] = tempSr.values    
+
+print(sensor_model)        
     
     
